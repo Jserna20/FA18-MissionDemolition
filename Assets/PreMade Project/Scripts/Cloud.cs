45fe7430a65aba5dd7b58f20812c732a10315046
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2018-09-21 17:42:12 -0400

    Making the castle

--- Assets/PreMade Project/Scripts/Cloud.cs
+++ Assets/PreMade Project/Scripts/Cloud.cs
@@ -49,10 +49,10 @@ public class Cloud : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown (KeyCode.Space))
+     /*   if (Input.GetKeyDown (KeyCode.Space))
         {
 			Restart ();
-		}
+		}*/
 	}
 	void Restart ()
     {
